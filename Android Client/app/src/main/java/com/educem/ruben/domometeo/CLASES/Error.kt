14a3a693b1cd5efc093760a5edc2bbe35d03df4c
Ruben Arrebola <ruben.arre6@gmail.com>
2017-06-04 14:30:08 +0200

    Se añaden los archivos iniciales para el repositorio, a partir de aqui se empiezan a añadir las nuevas ramas para cada versión, hasta ahora solo contiene el directorio Android, con la nueva aplicación Android, tenemos echo el Login, y el registro esta en proceso.

---
+++ Android Client/app/src/main/java/com/educem/ruben/domometeo/CLASES/Error.kt
@@ -0,0 +1,11 @@
+package com.educem.ruben.domometeo.CLASES
+
+import android.app.PendingIntent.getActivity
+import com.educem.ruben.domometeo.R
+
+/**
+ * Created by ruben on 3/06/17.
+ */
+class Error (var error : Boolean, var numero : Int, var descripcion : String ) {
+
+}
